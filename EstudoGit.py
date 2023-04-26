4078857591b951260340940a8ae02a42764a5aa2
Grifths <120991634+Grifths@users.noreply.github.com>
2023-04-26 09:19:01 -0300

    Criando print olá mundo

---
+++ EstudoGit.py
@@ -0,0 +1,7 @@
+#git status é comando para ve se o comando está no portifolio. Pois só estándo no portifolio q ele pode ser salva
+
+print("Olá mundo") #Se aparecer Untracked files signfinca q não está no git
+
+#Comando git add . para adicionar! Com o "." ele adiciona todos os arquivos, se não quiser é só colocar o nome do arquivo especifico
+#git commit para comitar nosso código! E utiliza o -a(Adicionar todo tipo de arquivo ao commit) -m(Ele vc adiciona uma mensagem para alguém tenha um norte ao ver seu commit)
+#Exemplo de como fica: git commit -a -m "Criando print olá mundo"
