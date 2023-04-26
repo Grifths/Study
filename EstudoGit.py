bfa08a485300c25a7fd76d66459758d7fcf3c072
Grifths <120991634+Grifths@users.noreply.github.com>
2023-04-26 10:47:18 -0300

    salvando

--- EstudoGit.py
+++ EstudoGit.py
@@ -5,3 +5,8 @@ print("Olá mundo") #Se aparecer Untracked files signfinca q não está no git
 #Comando git add . para adicionar! Com o "." ele adiciona todos os arquivos, se não quiser é só colocar o nome do arquivo especifico
 #git commit para comitar nosso código! E utiliza o -a(Adicionar todo tipo de arquivo ao commit) -m(Ele vc adiciona uma mensagem para alguém tenha um norte ao ver seu commit)
 #Exemplo de como fica: git commit -a -m "Criando print olá mundo"
+
+# digitando git branch conseguimos observar a lista de brach! Para poder acessalos e o que está em verde é oq eu estou
+
+#git merge test. o merge é oq uni os códigos, como estou no main ele vai unir o "teste" e jogar para o main
+#Só digitar git merge "nome do brench que deseja juntar"
